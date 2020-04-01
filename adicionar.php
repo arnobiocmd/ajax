d38fc528cd16a2133589763750a5a0278d5d5c55
Arnobio Fernandes <arnobiopaulinofernandes@gmail.com>
2020-03-31 21:37:57 -0300

    inicaido projeto ajax segundo commit

---
+++ adicionar.php
@@ -0,0 +1,20 @@
+<div class="form-box"></div>
+		<form method="POST">
+			<div class="form-box-insed">
+				<label>Nome</label>
+				<input type="text" name="nome">
+			</div>
+			<div class="form-box-insed">
+				<label>E-mail</label>
+				<input type="email" name="email">
+			</div>
+			<div class="form-box-insed">
+				<label>Senha</label>
+				<input type="password" name="senha">
+			</div>
+			<div class="form-box-insed">
+				<input type="submit" name="acao" value="Enviar">
+			</div>
+			
+		</form>
+		</div>
--- index.php
+++ index.php
@@ -9,7 +9,20 @@
 	<!DOCTYPE html>
 	<html>
 	<head>
-		<title></title>
+		<a href="adicionar.php"><button>Adicionar</button></a>
+		<table width="600" border="1">
+			<tr>
+				<th>Nome</th>
+				<th>E-mail</th>
+				
+			</tr>
+			<tr>
+				<td>#</td>
+				<td>#</td>
+				
+			</tr>
+			
+		</table>
 	</head>
 	<body>
 	
